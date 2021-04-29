947c6367377fad4e3a518aae3cf387d5e1157c4c
peixingzhe <peixingzhe@163.com>
2021-04-29 17:21:10 +0800

    111

--- user/src/main/java/com/rhythm/user/mapper/UserMapper.java
+++ user/src/main/java/com/rhythm/user/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.rhythm.user.mapper;
 
 import com.rhythm.user.entity.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -23,4 +24,9 @@ public interface UserMapper extends BaseMapper<User> {
     @Select("select r.name from role r where r.id in(select roleId from user_role where userId = #{userId})")
     HashSet<String> getRoles(@Param("userId") Integer userId);
 
+    @Insert("insert into user_dining values(#{userId, #{diningId})")
+    void addRelation(@Param("userId") Integer userId, @Param("diningId") Integer diningId);
+
+    @Delete("delete from user_dining where userId = #{userId}")
+    void delRelation(@Param("userId") Integer userId);
 }
